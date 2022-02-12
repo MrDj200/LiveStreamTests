186b22c8ca52d1f31a01a161069af0096d66311e
MrDj200 <david@klatt-luedenscheid.de>
2022-02-12 16:34:17 +0100

    Disabled Example Endpoint

--- CoreWebAPI/Controllers/WeatherForecastController.cs
+++ CoreWebAPI/Controllers/WeatherForecastController.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CoreWebAPI.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
+    //[ApiController]
+    //[Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
